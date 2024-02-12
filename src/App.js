34778ceff3ed5ec9ec497eb67f181d99a5a06c91
Jean <jean_pierre_97@outlook.com>
2024-02-12 11:55:16 -0300

    aula 21 Props e organização parte 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,74 +1,32 @@
 import './App.css';
 import { Component } from 'react';
+import { loadPosts } from './utils/load-posts';
+import { Posts } from './components/posts';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      posts: []
-    }
+  state = {
+    posts: []
   }
-
+  
   componentDidMount() {
     this.loadPosts()
   }
-
+  
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url}
-    })
-
+    const postsAndPhotos = await loadPosts()
     this.setState({ posts: postsAndPhotos })
   }
-
+  
   render() {
     const {posts} = this.state
     return (
       <section className='container'>
-        <div className="posts">
-          {posts.map(post => (
-            <div className='post'>{console.log(post.cover)}
-              <img src={post.cover} alt={post.title}/>
-              <div key = {post.id} className='post-content'>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-                </div>
-            </div>
-          ))}
-        </div>
+        <Posts posts = {posts}/>
       </section>
-
-    );
-  }
-}
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
+        
+        );
+      }
+    }
+    
+    export default App;
+    
